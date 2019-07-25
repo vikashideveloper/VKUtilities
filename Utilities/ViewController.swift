58d1c67a14b845e781acbe87e01d46770a7d3763
Vikash Kumar <vikash.ideveloper@gmail.com>
2019-07-25 14:35:38 +0530

    Initial Commit

---
+++ Utilities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Utilities
+//
+//  Created by Vikash Kumar on 25/07/19.
+//  Copyright © 2019 Intelivita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
